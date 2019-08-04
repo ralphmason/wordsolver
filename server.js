d46d573d07c77ac2b5ba524960daf818de489099
Ralph Mason <ralph.mason@telogis.com>
2019-08-04 09:22:53 -0500

    don't use __dirname

--- server.js
+++ server.js
@@ -1,125 +1,125 @@
-
-var path = require('path');
-var express = require('express');
+var path = require("path");
+var express = require("express");
 var bodyParser = require("body-parser");
-var fs=require('fs');
+var fs = require("fs");
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var staticPath = path.join(__dirname, '/web');
-app.use(express.static(staticPath,{index:'index.html'}));
+var working_dir = process.cwd();
 
-var solver=require('./solver');
+var staticPath = path.join(working_dir, "/web");
+app.use(express.static(staticPath, { index: "index.html" }));
 
-app.post("/result",(res,rep)=>{
-    var prob=res.body;
+var solver = require("./solver");
 
-    var data='';
+app.post("/result", (res, rep) => {
+  var prob = res.body;
 
-    for(var r=0;r<8;r++){
-        for(var c=0;c<8;c++){
-            data+=prob[(r*8)+c] ||'';
-            if(c<7){
-                data+=','
-            }
-        }
-        if ( r <7){
-            data +='\r';
-        }
-    }
-    solver(data,(sol)=>{
-        rep.send(sol);
-        rep.end();
-    });
+  var data = "";
 
-})
-
-app.post("/hilight",(res,rep)=>{
-    var prob=res.body.data;
-    var word=res.body.word;
-
-    word=word.replace(/qu/g,'~');
-   
-
-    var data='';
-
-    for(var r=0;r<8;r++){
-        for(var c=0;c<8;c++){
-            data+=prob[(r*8)+c] ||'';
-            if(c<7){
-                data+=','
-            }
-        }
-        if ( r <7){
-            data +='\r';
-        }
-    }
-
-    solver(data,word,(sol)=>{  
-        function find(x){
-            for(var i =0;i<x.length;i++){
-                var ele=x[i];
-                if (Array.isArray(ele)){
-                    if(ele.length==64){
-                        return ele;
-                    }
-                        
-                    var ret=find(ele);
-
-                    if (ret){
-                        return ret;
-                    }
-                }
-            }
-            return null;
-        }
-        var toSend=find(sol);
-        rep.send(toSend||{});
-        rep.end();
-    
-    });
-});
-
-
-app.post('/save',(res,rep)=>{
-    fs.writeFile(path.join(__dirname, `/saved/${res.body.level}.json`), JSON.stringify(res.body.layout), ()=>{
-        rep.send('saved');
-        rep.end();
-    });
-});
-
-app.post('/load',(res,rep)=>{
-    fs.readFile(path.join(__dirname, `/saved/${res.body.level}.json`), (err,data)=>{
-    if(data){
-        rep.send(JSON.parse(data? data.toString():{}));
+  for (var r = 0; r < 8; r++) {
+    for (var c = 0; c < 8; c++) {
+      data += prob[r * 8 + c] || "";
+      if (c < 7) {
+        data += ",";
       }
-        rep.end();
-    });
+    }
+    if (r < 7) {
+      data += "\r";
+    }
+  }
+  solver(data, sol => {
+    rep.send(sol);
+    rep.end();
+  });
 });
 
+app.post("/hilight", (res, rep) => {
+  var prob = res.body.data;
+  var word = res.body.word;
 
-app.post('/works',(res,rep)=>{
-    var word=res.body.word;
+  word = word.replace(/qu/g, "~");
 
-    fs.appendFile('good.txt',word+'\r', function (err) {
-       rep.end();
-      });
+  var data = "";
+
+  for (var r = 0; r < 8; r++) {
+    for (var c = 0; c < 8; c++) {
+      data += prob[r * 8 + c] || "";
+      if (c < 7) {
+        data += ",";
+      }
+    }
+    if (r < 7) {
+      data += "\r";
+    }
+  }
+
+  solver(data, word, sol => {
+    function find(x) {
+      for (var i = 0; i < x.length; i++) {
+        var ele = x[i];
+        if (Array.isArray(ele)) {
+          if (ele.length == 64) {
+            return ele;
+          }
+
+          var ret = find(ele);
+
+          if (ret) {
+            return ret;
+          }
+        }
+      }
+      return null;
+    }
+    var toSend = find(sol);
+    rep.send(toSend || {});
+    rep.end();
+  });
 });
-app.post('/didnt',(res,rep)=>{
-    var word=res.body.word;
 
-    fs.appendFile('bad.txt',word+'\r', function (err) {
-       rep.end();
-      });
+app.post("/save", (res, rep) => {
+  fs.writeFile(
+    path.join(working_dir, `/saved/${res.body.level}.json`),
+    JSON.stringify(res.body.layout),
+    () => {
+      rep.send("saved");
+      rep.end();
+    }
+  );
 });
 
+app.post("/load", (res, rep) => {
+  fs.readFile(
+    path.join(working_dir, `/saved/${res.body.level}.json`),
+    (err, data) => {
+      if (data) {
+        rep.send(JSON.parse(data ? data.toString() : {}));
+      }
+      rep.end();
+    }
+  );
+});
+
+app.post("/works", (res, rep) => {
+  var word = res.body.word;
+
+  fs.appendFile("good.txt", word + "\r", function(err) {
+    rep.end();
+  });
+});
+app.post("/didnt", (res, rep) => {
+  var word = res.body.word;
+
+  fs.appendFile("bad.txt", word + "\r", function(err) {
+    rep.end();
+  });
+});
 
 var port = 3000;
 
-
 app.listen(port, function() {
-  console.log('listening on'+port);
+  console.log("listening on" + port);
 });
-
